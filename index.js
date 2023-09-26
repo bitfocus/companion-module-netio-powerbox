df3550db13c18af99c2fb6ed481d1040d563f1a6
Joseph Adams <josephdadams@gmail.com>
2023-09-26 08:17:50 -0400

    formatting and variable scope fix

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ class BoxInstance extends InstanceBase {
 	constructor(internal) {
 		super(internal)
 	}
-    outputs = [];
+	outputs = [];
+
 	async init(config) {
 		this.config = config   
 		this.updateStatus('Connecting')
 
 		this.updateActions() // export actions
-        this.updateFeedbacks()
-        this.getStatus();
-        this.updatePresets();
-        this.updateStatus(InstanceStatus.Ok);
-
+		this.updateFeedbacks()
+		this.getStatus();
+		this.updatePresets();
+		this.updateStatus(InstanceStatus.Ok);
 	}
+
 	// When module gets deleted
 	async destroy() {
 		this.log('debug', 'destroy')
@@ -40,7 +41,6 @@ class BoxInstance extends InstanceBase {
 	}
 
 	updateActions() {
-		
 		this.setActionDefinitions({
 			setOutput: {
 				name: 'Set output on/off',
@@ -51,108 +51,152 @@ class BoxInstance extends InstanceBase {
 						id: 'output',
 						default: '1',
 						useVariables: true,
-					},{
-                        type: 'dropdown',
-                        label: 'select',
-                        id: 'para',
-                        choices: [ { id: '1', label: 'On'}, { id: '0', label: 'Off'} ],
-                        default: '1'
-                    }
+					},
+					{
+						type: 'dropdown',
+						label: 'select',
+						id: 'para',
+						choices: [ { id: '1', label: 'On'}, { id: '0', label: 'Off'} ],
+						default: '1'
+					}
 				],
 				callback: async (event) => {
 					const id = await this.parseVariablesInString(event.options.output)
-
 					this.setOutput(id, event.options.para)
 				},
 			},
-			
 		})
 	}
-    updateFeedbacks() {
-        this.setFeedbackDefinitions({
-            outputs: {
-                type: 'boolean',
-                name: 'Outputs',
-                description: 'Get the status of the outputs',
-                options: [
-                    {
-                        type:'number',
-                        label: 'Output',
-                        id: 'output',
-                        default: '1',
 
-                    }
-                ],
-                defaultStyle: {
-                    bgcolor: combineRgb(255,255,0),
-                },
-                callback: (feedback,) => {  
-                    if (this.outputs.length > 0) {
-                        let output = this.outputs.find(element => element.ID.toString() == feedback.options.output)
-                        if(output){
-                        return output.State == 1
-                        } else {
-                            return false
-                        }
-                    }
-                    return false
-                }}});
+	updateFeedbacks() {
+		this.setFeedbackDefinitions({
+			outputs: {
+				type: 'boolean',
+				name: 'Outputs',
+				description: 'Get the status of the outputs',
+				options: [
+					{
+						type:'number',
+						label: 'Output',
+						id: 'output',
+						default: '1',
 
-    }
-    async updatePresets() {
-        let presets = [];
-        let url = `http://${this.config.ip}/netio.json`;
-        let response = await axios.get(url).catch(error => {
-            this.log('error', error.message)
-        } );
-        this.outputs = response.data.Outputs;
-        this.outputs.forEach(element => {
-            presets.push({
-                type: 'button',
-                category: 'Outputs',
-                name: 'Output ' + element.ID,
-                style:{
-                    text:"Output "+element.ID+" ",
-                    color:combineRgb(255,255,255),
-                    size:'auto',
-                    bgcolor:combineRgb(0,0,0),
-                },
-                steps:[{down:[{actionId:'setOutput', options:{output:element.ID, para:1}}]},{down:[{actionId:'setOutput', options:{output:element.ID, para:0}}]}],
-                feedbacks: [{feedbackId:'outputs', options:{output:element.ID},style:{
-                    bgcolor: combineRgb(255,255,0),
-                    color: combineRgb(0,0,0),
-                }}]
+					}
+				],
+				defaultStyle: {
+					bgcolor: combineRgb(255,255,0),
+				},
+				callback: (feedback,) => {
+					if (this.outputs.length > 0) {
+						let output = this.outputs.find(element => element.ID.toString() == feedback.options.output)
+						if(output){
+						return output.State == 1
+						} else {
+							return false
+						}
+					}
+					return false
+				}
+			}
+		});
+	}
 
-        })});
-        this.setPresetDefinitions(presets);
-    }
-    async getStatus() {
-        let url = `http://${this.config.ip}/netio.json`;
-        let response = await axios.get(url).catch(error => {
-            this.log('error', error.message);
-            this.updateStatus(InstanceStatus.ConnectionFailure,error.message)} );
-        this.outputs = response.data.Outputs;
-        this.checkFeedbacks('outputs'); 
-    }   
-    async setOutput(output, para) {
-        let url = `http://${this.config.ip}/netio.json`;
-        try{
-        let body = JSON.parse(`{ "Outputs":[ { "ID":${output}, "Action":${para} } ] }`);
-        } catch (error) {
-            this.log('error', error.message);
-            this.updateStatus(InstanceStatus.UnknownError,error.message);
-        }
-        if(this.outputs.find(element => element.ID.toString() == output)){
-            
-        let response = await axios.post(url, body).catch(error => {
-            this.log('error', error.message)} );
-            
-        } else {
+	async updatePresets() {
+		let presets = [];
+		let url = `http://${this.config.ip}/netio.json`;
+		let response = await axios.get(url).catch(error => {
+			this.log('error', error.message)
+		} );
+		this.outputs = response.data.Outputs;
+		this.outputs.forEach(element => {
+			presets.push({
+				type: 'button',
+				category: 'Outputs',
+				name: 'Output ' + element.ID,
+				style:{
+					text:"Output "+element.ID+" ",
+					color:combineRgb(255,255,255),
+					size:'auto',
+					bgcolor:combineRgb(0,0,0),
+				},
+				steps:[
+					{
+						down:[
+							{
+								actionId:'setOutput',
+								options:
+									{
+										output: element.ID, para:1
+									}
+							}
+						]
+					},
+					{
+						down:[
+							{
+								actionId:'setOutput',
+								options:
+									{
+										output: element.ID, para:0
+									}
+							}
+						]
+					}
+				],
+				feedbacks: [
+					{
+						feedbackId:'outputs',
+						options:
+						{
+							output: element.ID
+						},
+						style:
+						{
+							bgcolor: combineRgb(255,255,0),
+							color: combineRgb(0,0,0),
+						}
+					}
+				]
+			})
+		});
+		
+		this.setPresetDefinitions(presets);
+	}
+
+	async getStatus() {
+		let url = `http://${this.config.ip}/netio.json`;
+		let response = await axios.get(url).catch(error => {
+			this.log('error', error.message);
+			this.updateStatus(InstanceStatus.ConnectionFailure,error.message)
+		});
+
+		this.outputs = response.data.Outputs;
+		this.checkFeedbacks('outputs'); 
+	}
+
+	async setOutput(output, para) {
+		let url = `http://${this.config.ip}/netio.json`;
+
+		let body = undefined;
+
+		try {
+			body = JSON.parse(`{ "Outputs":[ { "ID":${output}, "Action":${para} } ] }`);
+		}
+		catch (error) {
+			this.log('error', error.message);
+			this.updateStatus(InstanceStatus.UnknownError, error.message);
+		}
+		if (this.outputs.find(element => element.ID.toString() == output)) {
+			let response = await axios.post(url, body).catch(error => {
+				this.log('error', error.message)} );
+		}
+		else {
 			this.log('error', 'That output does not exist on this NETIO');
 		}
-        this.getStatus();
-    }
-    call
+
+		this.getStatus();
+	}
+	//call
 }
 
 runEntrypoint(BoxInstance, [])
